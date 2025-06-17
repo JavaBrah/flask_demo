6ca0a188f6bd5038b0b5d76e4acc8867c58f2bdd
Kegan <kegcliffjohnson@gmail.com>
2025-06-17 17:21:37 -0400

    added missing comments

--- api.py
+++ api.py
@@ -1,7 +1,8 @@
 # We import the `Flask` and `jsonify` classes from the Flask library.
 from models import *
 from flask import Flask, jsonify
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
+# Don't need this import, check models.py
 from sqlalchemy.orm import DeclarativeBase
 
 class Base(DeclarativeBase):
@@ -11,26 +12,11 @@ class Base(DeclarativeBase):
 # We create a Flask application by initializing the `app` object.
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg://kegan:linux@localhost/bd_example"
+#Imported db from models.py
 db.init_app(app)
 # db = SQLAlchemy(app)
 # Sample student data
-students = [
-     {'first_name': 'John', 'last_name': 'Doe', 'age': 18, 'grade': 'A'},
-     {'first_name': 'Jane', 'last_name': 'Smith', 'age': 19, 'grade': 'B'},
-     {'first_name': 'Bob', 'last_name': 'Johnson', 'age': 20, 'grade': 'C'},
-     {'first_name': 'Emily', 'last_name': 'Williams', 'age': 18, 'grade': 'A'},
-     {'first_name': 'Michael', 'last_name': 'Brown', 'age': 19, 'grade': 'B'},
-     {'first_name': 'Samantha', 'last_name': 'Davis', 'age': 22, 'grade': 'A'},
-     {'first_name': 'Oliver', 'last_name': 'Jones', 'age': 20, 'grade': 'B'},
-     {'first_name': 'Sophia', 'last_name': 'Miller', 'age': 21, 'grade': 'A'},
-     {'first_name': 'Ethan', 'last_name': 'Wilson', 'age': 19, 'grade': 'C'},
-     {'first_name': 'Isabella', 'last_name': 'Moore', 'age': 22, 'grade': 'B'}
- ]
 
-"""
--Views are bulit to receive requests and generate responses
--Route is a set url pattern that will route a request to the correct view
-"""
 
 
 @app.route("/api/v1/staff/", methods=['GET'])
